53c06688c440a45a5c927c1d1297b1006306adec
PioSQ <37858492+PioSQ@users.noreply.github.com>
2018-03-31 13:59:31 -0400

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -17,9 +17,6 @@ client.on("message", (message) => {
  } else
  if (message.content.startsWith(prefix + "keepplaylist")) {
    message.channel.send("Alright! Your playlist is saved. :apple:");
- } else
- if (meesage.content.startsWith(prefix + "playlists")) {
-   message.channel.send("https://www.youtube.com/playlist?list=PL6p1NYDZ87wIrjoY8APTIAcQf_l3_A7aj");
  }
 });
 
